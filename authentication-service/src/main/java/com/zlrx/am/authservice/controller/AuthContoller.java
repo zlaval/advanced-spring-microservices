87987bd2ae9e199644f26e81aa4b1111bd09dc75
zalan <zlaval@gmail.com>
2018-12-23 18:43:44 +0100

    configure resource server and auth server

---
+++ authentication-service/src/main/java/com/zlrx/am/authservice/controller/AuthContoller.java
@@ -0,0 +1,23 @@
+package com.zlrx.am.authservice.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthContoller {
+
+    @GetMapping(value = "/auth/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> user(OAuth2Authentication user) {
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("user", user.getUserAuthentication().getPrincipal());
+        userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+        return userInfo;
+    }
+
+}
